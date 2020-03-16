0b6aa7e942320bc39ed4d777b3fb24f895d719cd
Ansgar Hellwig <code@ahellwig.eu>
2020-03-16 09:45:26 +0100

    Update solution to VS 2019 and project to SDK style.

--- BibTeXLibrary/Properties/AssemblyInfo.cs
+++ BibTeXLibrary/Properties/AssemblyInfo.cs
@@ -1,19 +1,4 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-
-[assembly: AssemblyTitle("BibTeXLibrary")]
-[assembly: AssemblyDescription("A utility library for BibTeX.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BibTeXLibrary")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+﻿using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
-
 [assembly: Guid("e453ff0b-97b0-4575-9a2e-b15dcbf7c47f")]
-
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
